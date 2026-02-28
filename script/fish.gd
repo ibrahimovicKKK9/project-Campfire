a5a67aefca4ec0250feaef4ee7ec89062b4ec48d
haykal <ahmadalhaykal86@gmail.com>
2026-02-28 14:30:06 +0700

    nambahin fish

--- script/fish.gd
+++ script/fish.gd
@@ -1,8 +1,6 @@
 extends KinematicBody2D
 
 
-#player
-var player = null
 
 #speed
 export var speed = 100
@@ -12,22 +10,38 @@ export var speed = 100
 var direction  = -1
 
 
+var velocity = Vector2.ZERO
+
+
 #connect from area2d
 func _ready():
-	$Area2D.connect("body_entered",self,"_on_body_entered")
-	$Area2D.connect("body_exited",self,"_on_body_exited")
+	$Area2D.connect("body_entered", self, "_on_Area2D_body_entered")
+	print("Area monitoring:", $Area2D.monitoring)
 	
 # if player on area2d
-func _on_body_entered(body):
-	if	body.name == "player":
-		player = body
 
-# if player exit
-func _on_body_exited(body):
-	if body.name == "player":
-		player = null
+func _on_Area2D_body_entered(body):
+	if body.name == "Player":
+		get_tree().reload_current_scene()
+
+	
+
 		
 func _physics_process(delta):
+	velocity.x = direction * speed
+	velocity = move_and_slide(velocity)
 	
+	if $RayCast2D.is_colliding():
+		direction *= -1
+		$RayCast2D.cast_to.x *= -1
+		
+  
+
+
+
+
+
+
+
 
 
